c685be861af4622751707ef80097a26e6306096a
TonyGlow <TonyGlowable@outlook.com>
2025-04-08 18:22:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-document.getElementById("open-btn").addEventListener("click", function() {
-    document.getElementById("sidebar").style.width = "250px";
-});
+const menuBtn = document.getElementById("menu-btn");
+const sidebar = document.getElementById("sidebar");
 
-document.getElementById("close-btn").addEventListener("click", function() {
-    document.getElementById("sidebar").style.width = "0";
+menuBtn.addEventListener("click", function() {
+    if (sidebar.style.width === "250px") {
+        // Close the sidebar
+        sidebar.style.width = "0";
+        menuBtn.classList.remove("open");
+    } else {
+        // Open the sidebar
+        sidebar.style.width = "250px";
+        menuBtn.classList.add("open");
+    }
 });
